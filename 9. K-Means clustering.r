53f082594bf9c7347da9202fff6c5dab83e50a9a
Darsana R <darsanaramesh284@gmail.com>
2025-09-15 20:20:03 +0530

    Update 9. K-Means clustering.r

--- 9. K-Means clustering.r
+++ 9. K-Means clustering.r
@@ -6,9 +6,7 @@ scale_data=scale(usarrest)
 #when k=2
 result=kmeans(scale_data, centers=2, nstart=20)
 result$cluster
-
 plot(scale_data,col=(result$cluster), main="K-means clustering result with k=2",xlab="", ylab="", pch=20, cex=2)
-
 result
 result$centers
 result$tot.withinss
@@ -18,7 +16,6 @@ result2=kmeans(scale_data, centers=2, nstart=20)
 result2$cluster
 plot(scale_data,col=(result2$cluster), main="K-means clustering result with k=3",xlab="", ylab="", pch=20, cex=2)
 result2$tot.withinss
-
 
 #when k=4
 result3=kmeans(scale_data, centers=2, nstart=20)
